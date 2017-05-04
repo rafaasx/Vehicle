7fda4799c05f3ab37d25d7cdb320e2688b6e6a42
rafaasx <rafaeltwisted@gmail.com>
2017-05-04 15:06:52 -0300

    Finishi Him!

--- public/src/server.js
+++ public/src/server.js
@@ -8,7 +8,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '172202',
+    password : '',
     database : 'vehicle'
 });
 connection.connect(function(err) {
